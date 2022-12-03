f9067e8aacf813e0aac0ca8966902630aaae0791
Lars Mühmel <lars@muehml.eu>
2022-12-03 09:39:30 +0100

    make the nixpkgs module configurable

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -11,16 +11,72 @@
 # will be accepted into flake-parts, because it's against the
 # spirit of Flakes.
 #
+{ flake-parts-lib, lib, ... }:
+let
+  inherit (lib)
+    mkOption
+    types
+    mkOptionType
+    ;
+  inherit (flake-parts-lib)
+    mkSubmoduleOptions
+    ;
+  overlayType = mkOptionType {
+    name = "nixpkgs-overlay";
+    description = "nixpkgs overlay";
+    check = lib.isFunction;
+    merge = lib.mergeOneOption;
+  };
+in
 {
   config = {
-    perSystem = { inputs', lib, ... }: {
-      config = {
-        _module.args.pkgs = lib.mkOptionDefault (
-          builtins.seq
-            (inputs'.nixpkgs or (throw "flake-parts: The flake does not have a `nixpkgs` input. Please add it, or set `perSystem._module.args.pkgs` yourself."))
-            inputs'.nixpkgs.legacyPackages
-        );
+    perSystem = { config, inputs', ... }:
+      let
+        cfg = config.nixpkgs;
+      in
+      {
+        options = {
+          nixpkgs = mkSubmoduleOptions {
+            buildPlatform = mkOption {
+              type = types.either types.attrs types.str;
+              default = builtins.seq
+                (inputs'.nixpkgs or (throw "flake-parts: The flake does not have a `nixpkgs` input. Please add it, or set `perSystem._module.args.pkgs` yourself."))
+                inputs'.nixpkgs.legacyPackages.buildPlatform;
+            };
+            hostPlatform = mkOption {
+              type = types.either types.attrs types.str;
+              default = builtins.seq
+                (inputs'.nixpkgs or (throw "flake-parts: The flake does not have a `nixpkgs` input. Please add it, or set `perSystem._module.args.pkgs` yourself."))
+                inputs'.nixpkgs.legacyPackages.hostPlatform;
+            };
+            path = mkOption {
+              type = types.path;
+              default = builtins.seq
+                (inputs'.nixpkgs or (throw "flake-parts: The flake does not have a `nixpkgs` input. Please add it, or set `perSystem._module.args.pkgs` yourself."))
+                inputs'.nixpkgs.legacyPackages.path;
+              defaultText = "inputs'.nixpkgs.legacyPackges.path";
+            };
+            # can't be named config since it causes flake-parts to interpret the module differently
+            settings = mkOption {
+              type = types.attrs;
+              default = { };
+            };
+            overlays = mkOption {
+              type = types.listOf overlayType;
+              default = [ ];
+            };
+          };
+        };
+        config = {
+          _module.args.pkgs = lib.mkOptionDefault (
+            import cfg.path {
+              inherit (cfg) overlays;
+              config = cfg.settings;
+              localSystem = cfg.buildPlatform;
+              crossSystem = cfg.hostPlatform;
+            }
+          );
+        };
       };
-    };
   };
 }
